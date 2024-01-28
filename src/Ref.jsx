f99892bd1eeccea6d509792e163d8a8a44c9f2d3
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-01-28 20:55:06 +0200

    added ref

---
+++ src/Ref.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef } from 'react';
+
+const WithRef = () => {
+  const inputEl = useRef(null);
+  console.log(inputEl);
+
+  useEffect(() => {
+    console.log(inputEl);
+  });
+  return (
+    <div>
+      <input placeholder="name" ref={inputEl} />
+    </div>
+  );
+};
+
+export default WithRef;
